55c8fdfb9fb0c50c2f57cc55a94f7016c799c550
pierlucamario_dussich <pierluca.dussich@gmail.com>
2020-11-19 16:14:53 +0100

    VUE -TODOLIST + BONUS

--- js/script.js
+++ js/script.js
@@ -3,16 +3,28 @@ const newApp = new Vue({
    data:{
     inputToDo : [],
     textInput : '',
-    message: document.getElementById('message'),
+    active : [],
+    message: '',
    },
    methods:{
      inputInArray: function(){
-
+       if (this.textInput === ''){
+         this.message = 'Attento! Inserisci un ToDo'
+       } else {
          this.inputToDo.push(this.textInput);
-      
+         this.active.push(false);
+         this.message = " Hai " + this.inputToDo.length + " cose da fare"
+       }
      },
      removeInput: function(delIndex){
        this.inputToDo.splice(delIndex, 1);
+       this.active.splice(delIndex, 1);
+       this.message = " Hai " + this.inputToDo.length + " cose da fare"
      },
+
+     activate(index){
+       this.$set(this.active, index, !this.active[index]);
+     }
+
    }
 })
